c723a4e2d8cdecc1d58c601f1acd7a6e573b9498
Themis <midinfotn401@gmail.com>
2020-09-16 12:55:23 +0100

    fixed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ printf("Size of a char: %ld byte(s)\n", sizeof(charType));
 printf("Size of an int: %ld byte(s)\n", sizeof(intType));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(longintType));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongintType));
-printf("Size of float: %ld byte(s)\n", sizeof(floatType));
+printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
 return (0);
 }
