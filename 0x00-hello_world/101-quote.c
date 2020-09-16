f341c3686b6772b7e9b903aadb4e68b75680921c
Themis <midinfotn401@gmail.com>
2020-09-16 13:19:23 +0100

    fixed

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
