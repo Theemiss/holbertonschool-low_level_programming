5b606d71df631602f7e2bd6bfc106c206805c48e
Themis <midinfotn401@gmail.com>
2020-09-16 13:24:44 +0100

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (1);
+    printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+    return (1);
 }
