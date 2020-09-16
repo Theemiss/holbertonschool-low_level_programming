ede666f45feca1d8986deb33c55ed3cf848607cc
Themis <midinfotn401@gmail.com>
2020-09-16 13:39:45 +0100

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,6 +8,6 @@
  */
 int main(void)
 {
-    printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-    return (1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
