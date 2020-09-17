c4489559c4c297860c4db6f7abee72de7dd89644
Themis <midinfotn401@gmail.com>
2020-09-17 11:33:53 +0100

    update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+for (a = 48; a < 58; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
