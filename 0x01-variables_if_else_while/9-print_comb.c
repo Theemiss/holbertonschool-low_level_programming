409c2a5da05244563e07ffb891412e211ae0dbf6
Themis <midinfotn401@gmail.com>
2020-09-17 17:12:17 +0100

    new

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a;
+a = 48;
+while  (a < 58)
+{
+putchar(a);
+if (a !=  57)
+{
+putchar(44);
+putchar(32);
+a++;
+}
+else
+{
+a++;
+}
+}
+putchar('\n');
+return (0);
+}
