ff90d0ea8390c5787497584aa6bd11ca142b7fa1
Themis <midinfotn401@gmail.com>
2020-09-17 11:24:28 +0100

    update

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+printf("%i", n);
+}
+putchar('\n');
+return (0);
+}
