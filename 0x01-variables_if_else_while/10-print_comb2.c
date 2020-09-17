18cc4f84f74ff78bd37c5a97e2a7b69dc09f2eaf
Themis <midinfotn401@gmail.com>
2020-09-17 17:59:13 +0100

    finished

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,32 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i, j;
+for (i = 48; i <=  57; i++)
+{
+j = 48;
+while (j <= 57)
+{
+putchar(i);
+putchar(j);
+if ((i == 57) && (j == 57))
+{
+putchar('\n');
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+j++;
+}
+}
+return (0);
+}
