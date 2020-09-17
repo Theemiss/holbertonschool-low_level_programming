0043ee63ff40b77e1fa0a5c9a77d21b1c3498e02
Themis <midinfotn401@gmail.com>
2020-09-17 11:16:46 +0100

    update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+c = 'a';
+while (c <= 'z')
+{
+if ((c !=  'e') && (c != 'q'))
+{
+putchar(c);
+++c;
+}
+else
+{
+++c;
+}
+}
+putchar('\n');
+return (0);
+}
+
