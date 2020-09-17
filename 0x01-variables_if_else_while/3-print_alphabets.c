2ecf4b241255a9c5e80f4326c5fca74d5448448d
Themis <midinfotn401@gmail.com>
2020-09-17 10:53:42 +0100

    update

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+++c;
+}
+c = 'A';
+while (c <= 'Z')
+{
+putchar(c);
+++c;
+}
+putchar('\n');
+return (0);
+}
