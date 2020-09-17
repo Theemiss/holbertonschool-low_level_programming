8ecbf8805b3e7b175ad324e730feb4693e810e45
Themis <midinfotn401@gmail.com>
2020-09-17 11:49:11 +0100

    update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+int n;
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+for (c = 'a'; c  <= 'f'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
