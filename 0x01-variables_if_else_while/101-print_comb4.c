b4ae172e23509877965d275a5b8fca06d26ead8e
Themis <midinfotn401@gmail.com>
2020-09-18 21:53:15 +0100

    update

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j, y;
+i = 48;
+j = 49;
+y = 50;
+while  ((i < 56) && (j < 57) && (y < 58))
+{
+putchar(i);
+putchar(j);
+putchar(y);
+if ((i == 55) && (j == 56) && (y == 57))
+{
+putchar('\n');
+i++;
+j++;
+y++;
+}
+else
+{
+putchar(',');
+putchar(' ');
+if (y < 57)
+{
+y++;
+}
+else if ((j < 56) && (y == 57))
+{
+y = 1 + ++j;
+}
+else
+{
+j = ++i + 1;
+y = j + 1;
+}
+}
+}
+return (0);
+}
