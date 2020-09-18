a4095bcc5fa1696f63e9b9930b8f8f379037347c
Themis <midinfotn401@gmail.com>
2020-09-18 21:13:19 +0100

    update

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+i = 48;
+j = 49;
+while (i < 58)
+{
+putchar(i);
+putchar(j);
+if ((i == 56) && (j == 57))
+{
+putchar('\n');
+i++;
+j++;
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+if (j < 57)
+{
+j++;
+}
+else
+{
+i++;
+j = i + 1;
+}
+}
+return (0);
+}
