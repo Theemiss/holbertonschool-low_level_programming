41dc22169e15d1085729d1212269fab0d186e8e1
Themis <midinfotn401@gmail.com>
2020-09-21 11:35:50 +0100

    fixed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,17 @@
 #include "holberton.h"
 
+
+/**
+ * print alphabet - check the code for Holberton School students.
+ * Description: Prints the alphabet with _putchar
+ * Return: Always 0.
+ */
 void print_alphabet(void)
 {
-  char c;
-  for (c = 'a'; c <= 'z'; c++)
-    {
-      _putchar(c);
-    }
-  _putchar('\n');
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
