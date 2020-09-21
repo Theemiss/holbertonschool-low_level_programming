e5f6119534e24460b5c1ac9af9460405c2e8237e
Themis <midinfotn401@gmail.com>
2020-09-21 12:02:11 +0100

    update

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+if ((c >= 97) && (c <= 122))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
