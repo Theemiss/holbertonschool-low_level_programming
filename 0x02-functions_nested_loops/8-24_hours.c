7213e66734f2eedb6c2c25f959019b91a4a55a70
Themis <midinfotn401@gmail.com>
2020-09-21 14:44:13 +0100

    fixed

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,25 +1,27 @@
 #include "holberton.h"
 
-
-
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 void jack_bauer(void)
 {
-  int i, j, k, y, m, t;
-
-  for (i = 0; i < 24 ;i++)
-    {
-      k = i / 10;
-      y = i % 10;
-      for (j = 0; j < 60;j++)
-	{
-	 m = j / 10;
-			t = j % 10;
-			_putchar('0' + k);
-			_putchar('0' + y);
-			_putchar(':');
-			_putchar('0' + m);
-			_putchar('0' + t);
-			_putchar('\n');
-		}
-	}
-    }
+int i, j, k, y, m, t;
+for (i = 0; i < 24 ; i++)
+{
+k = i / 10;
+y = i % 10;
+for (j = 0; j < 60; j++)
+{
+m = j / 10;
+t = j % 10;
+_putchar('0' + k);
+_putchar('0' + y);
+_putchar(':');
+_putchar('0' + m);
+_putchar('0' + t);
+_putchar('\n');
+}
+}
+}
