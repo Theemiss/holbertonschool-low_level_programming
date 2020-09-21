e02535b1d1b1ab78ea5c82bda13b64dcb2157194
Themis <midinfotn401@gmail.com>
2020-09-21 12:54:46 +0100

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _abs(int x)
+{
+  if (x > 0)
+    {
+      return x;
+    }
+  else if ( x < 0)
+    {
+      return x= x * -1;
+      
+    }
+  else
+    return (0);
+}
