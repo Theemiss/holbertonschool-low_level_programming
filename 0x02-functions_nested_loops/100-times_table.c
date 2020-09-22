43e49e8b823f7df4f567fcc859d1580ba55b137e
Themis <midinfotn401@gmail.com>
2020-09-22 18:11:57 +0100

    fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,8 +8,8 @@
 void print_times_table(int n)
 {
 int i, j, k;
-if (n < 0 || n > 15)
-return;
+if (n >= 0 && n <= 15)
+{
 for (i = 0; i <= n; i++)
 {
 _putchar('0');
@@ -46,5 +46,6 @@ _putchar(',');
 }
 _putchar('\n');
 }
+} 
 }
 
