11a0b12eb8b8e48c6a955e3bbb329be043484f3b
Themis <midinfotn401@gmail.com>
2020-09-22 15:30:26 +0100

    fixed

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,16 +6,20 @@
  */
 int main(void)
 {
-int a, b, s;
-for (a = 0; a < 1024 - 3;)
+int a, b, s, x, y;
+for (a = 0; a <= (1024  / 3); a++)
 {
-a = a + 3;
-s = s + a;
+y = a * 3;
+s = s + y;
 }
-for (b = 0; b < 1024 - 5;)
+for (b = 0; b < (1024 / 5); b++)
 {
-s = s + b;
-b = b + 5;
+if (!(b % 3 == 0))
+{
+x = b * 5;
+s = s + x;
+}
 }
 printf("%i\n", s);
+return (0);
 }
