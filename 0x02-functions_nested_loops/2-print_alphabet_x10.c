713dc7f8462f7322975be8b8671ed9e65865fc7f
Themis <midinfotn401@gmail.com>
2020-09-22 10:02:49 +0100

    fixed

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * print_alphabet_x10 - print the alphabet 10 time
+ * Descreption: using _putchar
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
