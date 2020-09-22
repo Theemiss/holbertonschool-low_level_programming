fac5ed127749fd7b055229c9439f87f99af55532
Themis <midinfotn401@gmail.com>
2020-09-22 14:48:13 +0100

    update

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,22 +1,20 @@
 #include<stdio.h>
 /**
+ * main - count multiple.
  *
- *
- *
+ * return void
  */
 int main (void)
 {
-  int a, b;
-  a = 3;
-  b = 5;
-  while (a < 1024)
-    {
-    printf("%i\n", a);
-    a= 3 + a ;}
-  while (b < 1024)
-    {
-      printf("%i\n", b);
-            b = 5 + b;
-
-    }
+int a, b ,s;
+s = 0;
+a = 0;
+b = 0;
+while( s < 1024)
+{
+a = a + 3;
+b = b + 5;
+s = a + b;
+}
+printf("%i\n",s);
 }
