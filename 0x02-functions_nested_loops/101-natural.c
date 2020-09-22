ca6da581a9a8396cea9982773a5ae2ba6e41a262
Themis <midinfotn401@gmail.com>
2020-09-22 14:42:22 +0100

    update

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ *
+ *
+ *
+ */
+int main (void)
+{
+  int a, b;
+  a = 3;
+  b = 5;
+  while (a < 1024)
+    {
+    printf("%i\n", a);
+    a= 3 + a ;}
+  while (b < 1024)
+    {
+      printf("%i\n", b);
+            b = 5 + b;
+
+    }
+}
