f6622c8ceebc8e075b85bc6246255fa58a95f21b
Themis <midinfotn401@gmail.com>
2020-09-22 18:50:37 +0100

    final task

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+/**
+ * main - first 50 fibonatchi.
+ *
+ * Return: void.
+ */
+int main(void)
+{
+unsigned long int i, i0, i1, j, j0, j1, e, e0, e1, y, k;
+i = 1;
+	j = 2;
+	e = i + j;
+
+	printf("%lu, ", i);
+	printf("%lu, ", j);
+	for (y = 3; y < 89; y++)
+	{
+		printf("%lu, ", e);
+		i = j;
+		j = e;
+		e = i + j;
+	}
+	j0 = j / 1000000000;
+	j1 = j % 1000000000;
+	e0 = e / 1000000000;
+	e1 = e % 1000000000;
+	for (k = 89; k < 98; k++)
+	{
+		printf("%lu%lu, ", e0, e1);
+		i0 = j0;
+		i1 = j1;
+		j0 = e0;
+		j1 = e1;
+		e0 = i0 + j0 + ((i1 + j1) / 1000000000);
+		e1 = (i1 + j1) % 1000000000;
+	}
+printf("%lu%lu\n", e0, e1);
+	return (0);
+}
