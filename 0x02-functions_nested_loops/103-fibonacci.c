485443390de3add57f4902ceeaa5fdba5f1f2d2f
Themis <midinfotn401@gmail.com>
2020-09-22 18:32:32 +0100

    task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - even libber abbaci
+ *
+ * Return: void
+ */
+int main(void)
+{
+unsigned int i, j, y, k, s;
+i = 1;
+j = 2;
+y = 3;
+k = 2;
+for (s = 2; s <= 32; s++)
+{
+if (y % 2 == 0)
+{
+k = k + s;
+i = j;
+j = y;
+y = i + j;
+}
+}
+printf("%u\n", k);
+return (0);
+}
