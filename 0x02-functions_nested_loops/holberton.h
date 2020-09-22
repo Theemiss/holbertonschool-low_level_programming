bb2873b3505fbf5598e057ba55c1d3d78cdb0057
Themis <midinfotn401@gmail.com>
2020-09-22 18:14:02 +0100

    fix

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,4 +1,4 @@
-int *_putchar(char c);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -10,3 +10,4 @@ void jack_bauer(void);
 int add(int a, int b);
 void print_to_98(int n);
 void times_table(void);
+void print_times_table(int n);
