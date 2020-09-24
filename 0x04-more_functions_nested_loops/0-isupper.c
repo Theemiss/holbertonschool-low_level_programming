6f79b8c3287dff845dbd323c5488819bc6f39cad
Themis <midinfotn401@gmail.com>
2020-09-24 09:27:11 +0100

    check super case and library update

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _isupper - check uppercase character.
+ * @c: integer.
+ * Return: 0 succes.
+ */
+int _isupper(int c)
+{
+if ((c >= 65) && (c <= 90))
+{
+return (1);
+}
+else
+return (0);
+}
