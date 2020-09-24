d15ae9a6cd3cb22318b929da0a10b1d4a5e5e5e4
Themis <midinfotn401@gmail.com>
2020-09-24 09:53:02 +0100

    display number from 0 to 9 followed by new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - display all number from 0 to 9 followed by new line.
+ *
+ *Return: void
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
