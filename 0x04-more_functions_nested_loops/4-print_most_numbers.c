d9b056b82d0e7dfd5dca7a3853f6ea6ebcd3c288
Themis <midinfotn401@gmail.com>
2020-09-24 10:01:52 +0100

    print alsmost all the number expect 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - print all number from 0 to 9 expext 2 and 4.
+ *
+ * Return:void
+ */
+void print_most_numbers(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+if ((i != 50) && (i != 52))
+_putchar(i);
+}
+_putchar('\n');
+}
