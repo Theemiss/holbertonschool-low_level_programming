b0769814aa49decbbff9378dd45e468989d5263b
Themis <midinfotn401@gmail.com>
2020-09-24 20:38:15 +0100

    print numbrt putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,13 +6,15 @@
  */
 void print_number(int n)
 {
+	unsigned int x = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		x = -x;
 	}
-	if ((n / 10) > 0)
-		print_number(n / 10);
+	if ((x / 10) > 0)
+		print_number(x / 10);
 
-	_putchar(n % 10 + '0');
+	_putchar(x % 10 + '0');
 }
