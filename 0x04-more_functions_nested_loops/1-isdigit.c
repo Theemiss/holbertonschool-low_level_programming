ad96b9ca79a9aa8fcd426c04aa4e0ae765b87b9b
Themis <midinfotn401@gmail.com>
2020-09-24 09:35:39 +0100

    check digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* _isdigit - check digit.
+* @c: integer
+* Return: 1 succes
+*/
+int _isdigit(int c)
+{
+if ((c >= 48) && (c <= 57))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
