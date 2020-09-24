232615326a7061a2cb4fdfd2a8fe67e7dcef26fb
Themis <midinfotn401@gmail.com>
2020-09-24 20:30:00 +0100

    print numbrt putchar

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_number -print number putchar
+ * @n:integer
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar(n % 10 + '0');
+}
