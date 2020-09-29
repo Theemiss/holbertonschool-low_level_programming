74ff9aa0534a7a17a47ae9a37a3a65f51841bce9
Themis <midinfotn401@gmail.com>
2020-09-29 22:13:23 +0100

    print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_rev - print a string
+ * @s:pointer char
+ * return:void
+ */
+void print_rev(char *s)
+{
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	n = i;
+	for (i = n; i >= 0 ; i--)
+	{
+		_putchar(s[i]);
+	}
+_putchar('\n');
+return;
+}
