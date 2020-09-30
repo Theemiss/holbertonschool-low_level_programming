a3a8809e6c648e58772d9d6fdd5a0bb9ed8ba859
Themis <midinfotn401@gmail.com>
2020-09-30 11:26:45 +0100

    fixed

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,15 +8,11 @@
 
 int _atoi(char *s)
 {
-  int num = 0;
-  char digit;
-  while ((digit = *s++) != '\0') 
-  {
-    if (digit < '0' && digit > '9') {
-      return num; 
-    }
-    num *= 10;
-    num +=  digit - '0';
-  }
-  return num;
-}
+    int res = 0; 
+
+    for (int i = 0; s[i] != '\0'; ++i) 
+        res = res * 10 + s[i] - '0'; 
+  
+    return res; 
+} 
+
