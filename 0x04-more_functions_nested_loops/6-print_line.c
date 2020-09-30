2b2788e8c0a35d9fa0be41d51e8798d51a380cee
Themis <midinfotn401@gmail.com>
2020-09-30 10:44:47 +0100

    print a line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,11 +10,12 @@ void print_line(int n)
 	int i;
 
 	if (n != 0)
+	{
+	for (i = 0; i < n; i++)
 		{
-			for (i = 0; i < n; i++)
-		{
-			_putchar('_');
+		_putchar('_');
 		}
+	}
 	_putchar('\n');
 		}
 	else
