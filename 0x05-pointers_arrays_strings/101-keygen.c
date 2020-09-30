8b1a4ccf12e9573286f7f7838963ac8f0c5daa37
Themis <midinfotn401@gmail.com>
2020-09-30 21:24:18 +0100

    password generator

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - random password generator for 101-crackme
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+char a[100];
+	int r, n, i;
+
+	n = 0;
+	i = 0;
+	srand(time(NULL));
+	while (n < 2645)
+	{
+		r= rand() % 122;
+		if (r > 32)
+		{
+			a[i++] = r;
+			n += r;
+		}
+	}
+	a[i++] = (2772 - n);
+	a[i] = '\0';
+	printf("%s", a);
+
+	return (0);
+	
+}
