e46f1af7ebd011162baa7ebd9fb1943cec56ac73
Themis <midinfotn401@gmail.com>
2020-10-01 18:01:32 +0100

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * reverse_array - reverse array
+ * @a:array
+ * @n:integer
+ * Return:void
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j;
+	int x[n];
+
+	for (i = n - 1, j = 0; i >= 0; i--, j++)
+	{
+		x[j] = a[i];
+	}
+	for (i = 0; i < n; i++)
+	{
+		a[i] = x[i];
+	}
+}
