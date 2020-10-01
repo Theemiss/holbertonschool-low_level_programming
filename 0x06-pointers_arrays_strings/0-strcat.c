691fbca8a5cda1cb9bef43a2012e264765e51943
Themis <midinfotn401@gmail.com>
2020-10-01 11:19:49 +0100

    new files

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strcat -
+ * @dest:char
+ * @src:char
+ * Return:char
+ */
+char *_strcat(char *dest, char *src)
+{
+    char *s = dest;
+
+    while(*dest != '\0')
+    {
+        dest++;
+    }
+
+    while(*src != '\0')
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+
+    *dest = '\0';
+    return s;
+}
