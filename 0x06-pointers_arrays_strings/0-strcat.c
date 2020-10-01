58f22563f6f80e24be14f036afe892013d2174d1
Themis <midinfotn401@gmail.com>
2020-10-01 11:28:38 +0100

    concat 2 string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,26 @@
 #include "holberton.h"
 
 /**
- * _strcat -
+ * _strcat - concat 2 string
  * @dest:char
  * @src:char
  * Return:char
  */
 char *_strcat(char *dest, char *src)
 {
-    char *s = dest;
+	char *s = dest;
 
-    while(*dest != '\0')
-    {
-        dest++;
-    }
+	while (*dest != '\0')
+	{
+		dest++;
+	}
 
-    while(*src != '\0')
-    {
-        *dest = *src;
-        dest++;
-        src++;
-    }
-
-    *dest = '\0';
-    return s;
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (s);
 }
