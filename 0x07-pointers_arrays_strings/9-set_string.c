c9f91b66e3c81d49b136f1c65599136c4beb8e14
Themis <midinfotn401@gmail.com>
2020-10-05 15:21:13 +0100

    update

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * set_string - set a pointer to char
+ * @s:pointer to pointer
+ * @to:pointer to char
+ * Return:void;
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
