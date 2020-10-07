6da337c6b4ae4645b4d08f8babee0bb0ce98e0a6
Themis <midinfotn401@gmail.com>
2020-10-07 15:32:57 +0100

    update

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - power
+ * @x:int
+ * @y:int
+ * Return:int
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y != 0)
+		return (x * _pow_recursion(x, y - 1));
+
+	else
+	{
+		return (1);
+	}
+}
