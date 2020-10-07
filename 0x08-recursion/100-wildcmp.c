738abe715a6993b3bcbec2fb85a331339838b1d4
Themis <midinfotn401@gmail.com>
2020-10-07 16:43:16 +0100

    fixed

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -7,16 +7,24 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == *s2 && *s1 != '\0' && *s2 != '\0')
-		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
+	else if (*s1 == '\0' && *s2 == '*')
+		return (wildcmp(s1, (s2 + 1)));
+	else if (*s1 == '\0' || *s2 == '\0')
+		return (0);
+
+	if (*s1 == *s2)
+		return (wildcmp((s1 + 1), (s2 + 1)));
+
 	if (*s2 == '*')
 	{
-		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, (s2 + 1)));
+		else if (wildcmp(s1, s2 + 1))
 			return (1);
-		if (*(s1 + 1) != '\0' && *s2 == '\0')
-			return (0);
+		else
+			return (wildcmp(s1 + 1, s2));
 	}
 	return (0);
 }
