6e47ed9efbef9ddbd0ffbda323df3902e27ee4cd
Themis <midinfotn401@gmail.com>
2020-10-10 12:42:02 +0100

    update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - display name of program
+ * @argc:int
+ * @argv:string
+ * Return:int
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+
+		printf("%d\n", argc - 1);
+
+	return (0);
+}
