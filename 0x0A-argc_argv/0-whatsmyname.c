bb7c8df908af5e83a4d22897999dadc56d12f473
Themis <midinfotn401@gmail.com>
2020-10-10 12:34:38 +0100

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- * 
- * 
- * 
+ * main - display name of program
+ * @argc:int
+ * @argv:string
+ * Return:int
  */
 int main(int argc, char *argv[])
 {
 	if (argc == 1)
 	{
-			printf("%s\n",argv[0]);
+		printf("%s\n", argv[0]);
 	}
 	return (0);
 }
-
