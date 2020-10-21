79c7d4ca9cebdfe5e22706f66c94e652e822a8c5
AhmedBelhaj <midinfotn401@gmail.com>
2020-10-21 13:00:10 +0100

    update

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+/**
+ * int_index - Search for an integer
+ * @array:int
+ * @size:int
+ * @cmp:function
+ * Return:index of the integer searched for
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, x;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+		if (size != 0 && array != NULL)
+		{
+			for (i = 0; i < size; i++)
+			{
+				x = cmp(array[i]);
+				if (x != 0)
+					return (i);
+			}
+		}
+	return (-1);
+
+}
