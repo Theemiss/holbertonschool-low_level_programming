94e46a3b0db5a651c67492016f7ead2fa3062ca5
AhmedBelhaj <midinfotn401@gmail.com>
2020-10-21 12:32:27 +0100

    update

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - excute a function on array element
+ * @array: array
+ * @size: size_t
+ * @action:function to be excuted
+ * Return:void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	i = 0;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,1 +1,3 @@
 void print_name(char *name, void (*f)(char *));
+#include <stddef.h>
+void array_iterator(int *array, size_t size, void (*action)(int));
