3c7d6f5fc54a0ce4fcfedcd4c34ab7b7ebd5f3f7
Ahmed Belhaj <58787184+Theemiss@users.noreply.github.com>
2020-10-24 16:10:58 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef _FUNCTION_VARIADIC_H_
 #define _FUNCTION_VARIADIC_H_
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
