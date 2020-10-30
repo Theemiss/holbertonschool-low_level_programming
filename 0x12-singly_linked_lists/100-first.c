903110f6c0363d5d7381fd4005159af354011db8
Theemiss <58787184+Theemiss@users.noreply.github.com>
2020-10-30 14:33:34 +0100

    task 5 advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void printfirst (void) __attribute__ ((constructor));
+/**
+ * printfirst - print before main
+ *
+ * Return: void
+ */
+void printfirst (void) 
+{
+	printf ("You're beat! and yet, you must allow,\nI bore my house upon my back!\n"); 
+}
