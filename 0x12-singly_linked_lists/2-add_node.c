f949cd3e0b1508697dc8208babcafb7475f19325
Theemiss <58787184+Theemiss@users.noreply.github.com>
2020-10-30 13:02:39 +0100

    task 2 no betty

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * _strlen - lenght of string
+ * @s:char
+ * Return:int
+ */
+unsigned int _strlen(char *s)
+{
+	int i;
+
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			continue;
+		}
+return (i);
+}
+/**
+ * add_node - adding node 
+ * @head:last node list
+ * @str:string
+ * Return:new node created
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+if (str == NULL)
+	return(NULL);
+new = malloc (sizeof(list_t));
+if (new == NULL)
+		return (NULL);
+new->str = strdup(str);
+if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+new->len = _strlen(new->str);
+new->next = *head;
+*head = new;
+return(new);
+}
