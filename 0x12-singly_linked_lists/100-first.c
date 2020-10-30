cbfb3d77f11c9b93cd0ddd8eb3f1b77ba67d766f
Theemiss <58787184+Theemiss@users.noreply.github.com>
2020-10-30 14:36:25 +0100

    task 5 advanced betty my ass

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-void printfirst (void) __attribute__ ((constructor));
+void printfirst(void) __attribute__ ((constructor));
 /**
  * printfirst - print before main
  *
  * Return: void
  */
-void printfirst (void) 
+void printfirst(void) 
 {
-	printf ("You're beat! and yet, you must allow,\nI bore my house upon my back!\n"); 
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
