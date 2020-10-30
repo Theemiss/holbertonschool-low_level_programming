cc4cea1c501f76c5f3aa937083a2d4cfa713ad73
Theemiss <58787184+Theemiss@users.noreply.github.com>
2020-10-30 13:16:56 +0100

    fixed T2 and Betty

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -29,7 +29,9 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 new = malloc(sizeof(list_t));
 	if (new == NULL)
-				return (NULL);
+	{
+		return (NULL);
+	}
 new->str = strdup(str);
 	if (new->str == NULL)
 	{
