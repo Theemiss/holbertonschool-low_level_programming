84d6abd651679165a972d35db5295807dd919549
Theemiss <58787184+Theemiss@users.noreply.github.com>
2020-10-30 13:36:53 +0100

    task 3 no betty

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+/**
+ * _strlen - lenght of string
+ * @s:char
+ * Return:int
+ */
+unsigned int _strlen(char *s)
+{
+	int i;
+
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			continue;
+		}
+return (i);
+}
+/**
+ * add-node_end - add new node at the end of the list 
+ * @head:pointer to ponter to lined list
+ * @str : ponter to string inside the list 
+ * Return:pointer to a node at the end of the list 
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *last;
+
+	if (str == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = _strlen(new->str);
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+	last = *head;
+		while (last->next)
+		{
+		last = last->next;	
+		}
+	last->next = new;
+	}
+	return (new);
+}
