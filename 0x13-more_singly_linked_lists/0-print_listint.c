972bad0c2a4c415ba41bfff676ce2496ecc476a4
Theemiss <58787184+Theemiss@users.noreply.github.com>
2020-11-04 13:32:52 +0100

    task 0 no betty

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - print all element in a list 
+ * @h: pointer in a list
+ * Return:size of the list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t l = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		l++;
+		h = h->next;
+	}
+	return (l);
+}
