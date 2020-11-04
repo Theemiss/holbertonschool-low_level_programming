a531c9070062fc5cdc6168527a0d7ad90b774133
Theemiss <58787184+Theemiss@users.noreply.github.com>
2020-11-04 14:15:34 +0100

    betty

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,6 +8,7 @@
 size_t listint_len(const listint_t *h)
 {
 	size_t len = 0;
+
 	while (h)
 	{
 		len++;
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
- * 
- *
- *  
+ * add_nodeint_end - add node at the end of the list
+ *@head:pointer to last node
+ *@n:integer elment in node
+ * Return:new node
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
