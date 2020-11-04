0bb2a6d912e21873d3b4972fea390e1303bcda67
Theemiss <58787184+Theemiss@users.noreply.github.com>
2020-11-04 14:47:24 +0100

    task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,11 +10,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 size_t x;
 
-for(x = 0; x < index && head->next;x++)
+for (x = 0; x < index && head->next; x++)
 {
 	head = head->next;
 }
-if(x < index)
+if (x < index)
 	return (NULL);
 return (head);
 }
