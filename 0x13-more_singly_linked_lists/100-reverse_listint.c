79c1de1f91cc127943d034cadaecc526860e88fc
Theemiss <58787184+Theemiss@users.noreply.github.com>
2020-11-04 15:13:04 +0100

    task11

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a list
+ * @head: a pointer to the given list
+ *
+ * Return: a pointer to the newly reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *rev;
+	
+	if (head == NULL || (*head)->next == NULL)
+	{
+		return (*head);
+	}
+
+	rev = reverse_listint(&(*head)->next);
+	(*head)->next->next = *head;
+	(*head)->next = NULL;
+
+	return (rev);
+}
