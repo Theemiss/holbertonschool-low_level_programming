838c9294ca7a8623c177ea7de6ac627caeaae422
Theemiss <58787184+Theemiss@users.noreply.github.com>
2020-11-04 14:46:20 +0100

    task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - return nth node index
+ * @head:pointer to node
+ * @index:given index int
+ * Return:Null or index of node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+size_t x;
+
+for(x = 0; x < index && head->next;x++)
+{
+	head = head->next;
+}
+if(x < index)
+	return (NULL);
+return (head);
+}
