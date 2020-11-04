665ac7608bdc3d9e76de712035f288ed62757d73
Theemiss <58787184+Theemiss@users.noreply.github.com>
2020-11-04 13:58:25 +0100

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - length of a list
+ * @h:pointer to list element
+ * Return: size of list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t len = 0;
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+
+	return (len);
+}
