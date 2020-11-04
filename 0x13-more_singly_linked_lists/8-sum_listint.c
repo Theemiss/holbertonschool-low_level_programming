5060984ef1f1b98cbbde3770fe8d0d0836673b8f
Theemiss <58787184+Theemiss@users.noreply.github.com>
2020-11-04 14:54:13 +0100

    task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sum of element list
+ * @head:pointer to list
+ * Return: int
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+if (head == NULL)
+	return(0);
+
+while (head->next)
+{
+	sum += head->n;
+	head = head->next;
+}
+	sum += head->n;
+	return (sum);
+}
