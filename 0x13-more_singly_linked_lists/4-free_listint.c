38991c3def9167a62f80b8bda83dd07bf6edba57
Theemiss <58787184+Theemiss@users.noreply.github.com>
2020-11-04 14:24:23 +0100

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - free list
+ * @head:pointer to node element
+ * Return:void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *check, *c;
+
+c = head;
+while (c != NULL)
+{
+	check = c->next;
+	free(c->n);
+	free(c);
+	c = check;
+}
+}
