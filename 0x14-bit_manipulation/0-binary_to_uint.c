8ef60296587336cce5f8f9a6b4ccb5669fb63fdc
Theemiss <58787184+Theemiss@users.noreply.github.com>
2020-11-05 12:21:44 +0100

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+ * _strlen - lenght of string
+ * @s:char
+ * Return:int
+ */
+int _strlen(const char *s)
+{
+	int i;
+
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			continue;
+		}
+return (i);
+}
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+unsigned int binary_to_uint(const char *b)
+{
+ unsigned int k = 1;
+ unsigned int i=0;
+ int c;
+ unsigned int len;
+
+ len = _strlen(b);
+
+  for (c = len-1; c >= 0; c--)
+ {
+  if (b[c] != '0' && b[c] != '1')
+      return (0);
+    if (b[c] == '1')
+    {
+     i += k;
+     }
+    k *= 2;
+}
+ return (i);
+}
