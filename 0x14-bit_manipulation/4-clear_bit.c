6998ac0ab7927952bfd542ec074ad711dd3f0bba
Theemiss <58787184+Theemiss@users.noreply.github.com>
2020-11-05 13:24:41 +0100

    task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - set value to 1 with given index
+ * @n:pointer to unsigned int
+ * @index: unsigned int index
+ * Return: 1 if succes -1 in case of error
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int x = 1;
+
+	if (sizeof(n) * 8 < index)
+		return (-1);
+
+x <<= index;
+*n &= ~x;
+return (1);
+}
