1e74eb17b3feb10721384938943a71cc1d7fa27a
Theemiss <58787184+Theemiss@users.noreply.github.com>
2020-11-05 12:30:15 +0100

    betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -23,24 +23,25 @@ return (i);
  */
 unsigned int binary_to_uint(const char *b)
 {
- unsigned int k = 1;
- unsigned int i=0;
- int c;
- unsigned int len;
+	unsigned int k = 1;
+	unsigned int i=0;
+	int c;
+	unsigned int len;
 
 	if (b == NULL)
-		return(0);
- len = _strlen(b);
+		return (0);
 
-  for (c = len-1; c >= 0; c--)
- {
-  if (b[c] != '0' && b[c] != '1')
-      return (0);
-    if (b[c] == '1')
-    {
-     i += k;
-     }
-    k *= 2;
+	len = _strlen(b);
+
+	for (c = len-1; c >= 0; c--)
+	{
+	if (b[c] != '0' && b[c] != '1')
+		return (0);
+	if (b[c] == '1')
+	{
+		i += k;
+	}
+	k *= 2;
 }
- return (i);
+	return (i);
 }
