d59634ddeae81c39b10a9eb70d1af2b18bdc45db
Themis <midinfotn401@gmail.com>
2020-11-09 14:13:39 +0100

    updated lib

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
