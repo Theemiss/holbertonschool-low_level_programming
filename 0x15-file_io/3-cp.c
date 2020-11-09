458b447cddfb243e9b7dc626a554badfb01e6ccc
Themis <midinfotn401@gmail.com>
2020-11-09 15:02:24 +0100

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,43 +8,44 @@
  */
 int main(int ac, char *av[])
 {
-	int fd, rd, c, len;
-	int fd2, w, ps;
-	char buff[1024];
+	int fd, rw, cw, l;
+	int fd2, ww, cw2;
+	char *buff = malloc(sizeof(char) * 1024);
 
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-fd = open(av[1], O_RDONLY);
-rd = read(fd, buff, 1024);
-if (fd == -1 || rd == -1)
-{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-	exit(98);
-}
-c = close(fd);
-	for (len = 0; buff[len] != '\0'; len++)
-		;
-	if (c == -1)
+	fd = open(av[1], O_RDONLY);
+	rw = read(fd, buff, 1024);
+	if (fd == -1 || rw == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	cw = close(fd);
+	for (l = 0; buff[l] != '\0'; l++)
+	;
+	if (cw == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
-fd2 = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-w = write(fd2, buff, len);
-	if (fd2 == -1 || w == -1)
+
+	fd2 = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	ww = write(fd2, buff, l);
+	if (fd2 == -1 || ww == -1)
 	{
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-	exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
 	}
-ps = close(fd2);
-	if (ps == -1)
+	cw2 = close(fd2);
+	if (cw2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
 		exit(100);
 	}
-return (0);
+	free(buff);
+	return (0);
 }
