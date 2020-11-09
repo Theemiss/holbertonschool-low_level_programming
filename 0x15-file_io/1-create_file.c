530152fd8e41ba74b7a7313ac2a9907b0f97a165
Themis <midinfotn401@gmail.com>
2020-11-09 14:09:36 +0100

    maybe fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,11 +23,12 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len] != '\0'; len++)
 		;
 		w = write(fd, text_content, len);
-		if (w == -1)
+		
+	}
+	if (w == -1)
 		{
 			return (-1);
 		}
-	}
 	close(fd);
 	return (1);
 }
