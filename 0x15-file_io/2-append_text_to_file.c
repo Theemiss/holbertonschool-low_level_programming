c6aac9933235553d659a6d5e92e12d5649ae9375
Themis <midinfotn401@gmail.com>
2020-11-09 14:06:36 +0100

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,7 +8,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, len, w, x;
+	int fd, len, w;
 
 	if (filename == NULL)
 		return (-1);
@@ -23,10 +23,6 @@ int append_text_to_file(const char *filename, char *text_content)
 		if (w == -1)
 			return (-1);
 	}
-	x = close(fd);
-	if (x == -1)
-	{
-		return (-1);
-	}
+	close(fd);
 	return (1);
 }
