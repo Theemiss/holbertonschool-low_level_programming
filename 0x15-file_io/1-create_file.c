2541e393dec90a877e34a61e52f7419739a57813
Themis <midinfotn401@gmail.com>
2020-11-09 13:17:37 +0100

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ *  create_file - create a file and write in it
+ * @filename:file to be created
+ * @text_content: content of the file created
+ * Return: 1 or -1;
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, x, w;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR, 00600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		for (x = 0; text_content[x] != '\0'; x++)
+		;
+		w = write(fd, text_content, x);
+		if (w == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
