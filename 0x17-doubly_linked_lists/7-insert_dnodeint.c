4776bf74158d0b8540af0d56c6c890395b39e7e5
Themis <midinfotn401@gmail.com>
2020-12-13 16:29:12 +0100

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,8 +9,10 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-		unsigned int c = 0;
-		dlistint_t *start, *new;
+	unsigned int c = 0;
+	dlistint_t *start, *new;
+	unsigned int len = len_node(h);
+
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 	start = *h;
@@ -19,17 +21,40 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (c == idx - 1)
 			break;
 		else if (c < idx - 1 && start == NULL)
-			return (NULL);
+		return (NULL);
 		start = start->next;
 		c++;
 	}
 		new = malloc(sizeof(dlistint_t));
 		if (new == NULL)
 			return (NULL);
+	if (len == idx)
+		return (add_dnodeint_end(h, n));
+	else if (len < idx)
+		return (NULL);
 	start->next->prev = new;
 	new->next = start->next;
 	start->next = new;
 	new->prev = start;
 	new->n = n;
 	return (new);
+}
+/**
+ * len_node - list len
+ *
+ * @node:list
+ * Return:unsigned int
+ */
+unsigned int len_node(dlistint_t **node)
+{
+	unsigned int len = 0;
+	dlistint_t *start;
+
+	start = *node;
+	while (start != NULL)
+	{
+		len += 1;
+		start = start->next;
+	}
+	return (len);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -29,4 +29,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+unsigned int len_node(dlistint_t **node);
+
 #endif
