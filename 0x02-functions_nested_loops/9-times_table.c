de2601f9a13c745418292ebd70150d79dacceef1
Themis <midinfotn401@gmail.com>
2021-02-06 12:03:03 +0100

    styling

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,37 +8,38 @@
 
 void times_table(void)
 {
-int i, j, y, d, e;
-for (i = 0; i < 10; i++)
-{
-for (j = 0; j < 10; j++)
-{
-y = i * j;
-d = y / 10;
-e = y % 10;
-if (j == 0)
-{
-_putchar('0');
-}
-else if (y < 10)
-{
-_putchar(' ');
-_putchar('0' + e);
-}
-else
-{
-_putchar('0' + d);
-_putchar('0' + e);
-}
-if (j < 9)
-{
-_putchar(',');
-_putchar(' ');
-}
-else
-{
-_putchar('\n');
-}
-}
-}
+	int i, j, y, d, e;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			y = i * j;
+			d = y / 10;
+			e = y % 10;
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else if (y < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + e);
+			}
+			else
+			{
+				_putchar('0' + d);
+				_putchar('0' + e);
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
 }
