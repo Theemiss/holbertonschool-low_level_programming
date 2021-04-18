3a2d938d7e60752f30b8b5710ec87eb085082e1f
Themis <midinfotn401@gmail.com>
2021-04-19 00:47:11 +0100

    T 1 and init

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - implement  linear search  algo
+ * @array: given array to search in
+ * @size: size of the array
+ * @value: value to search for
+ * Return: first index or -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i <= size; i++)
+	{
+		if (array[i] == value)
+		{
+			printf("Value checked array[%li]= [%d]\n", i, array[i]);
+			return (i);
+		}
+		else
+		{
+			printf("Value checked array[%li]= [%d]\n", i, array[i]);
+		}
+	}
+	return (-1);
+
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef _SEARCH_H_
+#define _SEARCH_H_
+
+/** HEADERS */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/** Prototype */
+int linear_search(int *array, size_t size, int value);
+
+#endif
