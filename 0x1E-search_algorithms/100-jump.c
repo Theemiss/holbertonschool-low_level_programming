1b7ac83c61d158828cc3a0c3d2cc1afaf421e7e1
Themis <midinfotn401@gmail.com>
2021-04-20 22:31:30 +0100

    fix

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,39 +1,6 @@
 #include "search_algos.h"
 
 /**
- * _sqrt - _sqrt
- *
- * @r: value
- * Return: _sqrt of the value
- * Descreption:i didn t know that i can  use sqrt so i tried creating one sike
- */
-size_t _sqrt(size_t r)
-{
-	int start = 0, end = r;
-	int mid, res;
-
-	while (start <= end)
-	{
-		mid = (start + end) / 2;
-		if (mid * mid == r)
-		{
-			res = mid;
-			break;
-		}
-		if (mid * mid < r)
-		{
-			res = start;
-			start = mid + 1;
-		}
-		else
-		{
-			end = mid - 1;
-		}
-	}
-	return (res);
-}
-
-/**
  * jump_search - implement  jump search  algo
  * @array: given array to search in
  * @size: size of the array
